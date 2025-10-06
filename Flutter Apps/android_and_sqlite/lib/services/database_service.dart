872e34440fcddd38fe277e3e2c328a8f71f09181
ppfuark <phfuark2007@gmail.com>
2025-10-06 10:05:16 -0300

    FluterApps[feat]: SQLite
    Finshed: Simple ToDo App  can Read, Update, Create and Delete (with confirm alert) tasks

--- Flutter Apps/android_and_sqlite/lib/services/database_service.dart
+++ Flutter Apps/android_and_sqlite/lib/services/database_service.dart
@@ -1,3 +1,4 @@
+import 'package:android_and_sqlite/models/task.dart';
 import 'package:path/path.dart';
 import 'package:sqflite/sqflite.dart';
 
@@ -13,30 +14,66 @@ class DatabaseService {
   DatabaseService._constructor();
 
   Future<Database> get database async {
-    if(_db != null) return _db!;
+    if (_db != null) return _db!;
     _db = await getDatabase();
     return _db!;
   }
 
-  Future<Database> getDatabase() async{
+  Future<Database> getDatabase() async {
     final databaseDirPath = await getDatabasesPath();
     final databasePath = join(databaseDirPath, "tasks.db");
     final database = await openDatabase(
       databasePath,
-      onCreate: (db, version){
-        db.execute(
-          '''
+      version: 1,
+      onCreate: (db, version) {
+        db.execute('''
           CREATE TABLE $_tasksTableName (
             $_tasksIdColumnName INTEGER PRIMARY KEY,
             $_tasksContentColumnName TEXT NOT NULL,
             $_tasksStatusColumnName INTEGER NOT NULL
           )
-          '''
-        );
+          ''');
       },
     );
     return database;
   }
+
+  void addTask(String content) async {
+    final db = await database;
+
+    await db.insert(_tasksTableName, {
+      _tasksContentColumnName: content,
+      _tasksStatusColumnName: 0,
+    });
+  }
+
+  Future<List<Task>?> getTasks() async {
+    final db = await database;
+    final data = await db.query(_tasksTableName);
+    List<Task> tasks = data
+        .map(
+          (t) => Task(
+            id: t["id"] as int,
+            status: t["status"] as int,
+            content: t["content"] as String,
+          ),
+        )
+        .toList();
+    return tasks;
+  }
+
+  void updateTaskStatus(int id, int status) async {
+    final db = await database;
+    await db.update(
+      _tasksTableName,
+      {_tasksStatusColumnName: status},
+      where: 'id = ?',
+      whereArgs: [id],
+    );
+  }
+
+  void deleteTask(int id) async{
+    final db = await database;
+    await db.delete(_tasksTableName, where: 'id = ?', whereArgs: [id,]);
+  }
 }
-
-

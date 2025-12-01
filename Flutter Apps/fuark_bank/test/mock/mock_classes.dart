575d6973d6bc646e94ec9e7785e1913352312a58
ppfuark <phfuark2007@gmail.com>
2025-12-01 10:09:59 -0300

    FlutterApps[test]: Fuark Bank
    Mock test on signUp

---
+++ Flutter Apps/fuark_bank/test/mock/mock_classes.dart
@@ -0,0 +1,6 @@
+
+import 'package:fuark_bank/services/auth/auth_service.dart';
+import 'package:mocktail/mocktail.dart';
+
+class MockFirebaseAuthService extends Mock implements AuthService{}
+
---
+++ Flutter Apps/fuark_bank/test/services/firebase_auth_service_test.dart
@@ -0,0 +1,36 @@
+import 'package:flutter_test/flutter_test.dart';
+import 'package:fuark_bank/common/models/user_model.dart';
+import 'package:mocktail/mocktail.dart';
+
+import '../mock/mock_classes.dart';
+
+void main() {
+  late MockFirebaseAuthService mockFirebaseAuthService;
+  setUp(() {
+    mockFirebaseAuthService = MockFirebaseAuthService();
+  });
+
+  final user = UserModel(
+    name: "User",
+    email: "user@mail.com",
+    id: "1a2b3c4d5e",
+  );
+
+  test("Test sign up sucess", () async {
+    when(
+      () => mockFirebaseAuthService.signUp(
+        name: "User",
+        email: "user@mail.com ",
+        password: "User@123",
+      ),
+    ).thenAnswer((_) async => user);
+
+    final result = await mockFirebaseAuthService.signUp(
+      name: "User",
+      email: "user@mail.com ",
+      password: "User@123",
+    );
+
+    expect(result, user);
+  });
+}

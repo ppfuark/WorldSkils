0c57f7cb7c665c6fb1f08085c95fa4c45507f09d
ppfuark <phfuark2007@gmail.com>
2026-01-20 10:04:34 -0300

    FlutterApps[feat]: Chat App
    Auth Gate to check the state of the user (logged or not)

--- Flutter Apps/chat_app/lib/main.dart
+++ Flutter Apps/chat_app/lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:chat_app/feat/auth/login_or_register.dart';
 import 'package:chat_app/firebase_options.dart';
+import 'package:chat_app/services/auth_gate.dart';
 import 'package:chat_app/themes/light_mode.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
@@ -18,7 +19,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: LoginOrRegister(),
+      home: AuthGate(),
       theme: lightMode,
     );
   }

ab3bc16d6aab8c61ed2f9113a081d5bed56d2008
ppfuark <phfuark2007@gmail.com>
2026-01-20 09:04:28 -0300

    FlutterApps[feat]: Chat App
    Firebase

--- Flutter Apps/chat_app/lib/main.dart
+++ Flutter Apps/chat_app/lib/main.dart
@@ -1,8 +1,12 @@
 import 'package:chat_app/feat/auth/login_or_register.dart';
+import 'package:chat_app/firebase_options.dart';
 import 'package:chat_app/themes/light_mode.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
   runApp(const MyApp());
 }
 

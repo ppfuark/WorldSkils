3da456dc3e324e2475deef9d4e03021ca55c7ec5
ppfuark <phfuark2007@gmail.com>
2026-01-23 09:49:17 -0300

    MockTests[feat]: Estudy
    Rendering users | AppTile

---
+++ MockTests/2 - Estudy/estudy/lib/common/app_tile.dart
@@ -0,0 +1,118 @@
+import 'package:flutter/material.dart';
+
+class AppTile extends StatelessWidget {
+  final String title;
+  final String? subtitle;
+  final IconData icon;
+
+  final bool isEditable;
+  final Function()? onEdit;
+
+  final bool isDeletable;
+  final Function()? onDelete;
+
+  const AppTile({
+    super.key,
+    required this.title,
+    required this.icon,
+    required this.isEditable,
+    this.onEdit,
+    required this.isDeletable,
+    this.onDelete,
+    this.subtitle,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    final theme = Theme.of(context).colorScheme;
+
+    return Container(
+      margin: EdgeInsets.symmetric(vertical: 6),
+      width: double.infinity,
+      padding: EdgeInsets.symmetric(horizontal: 6, vertical: 10),
+      decoration: BoxDecoration(
+        color: Colors.white,
+        borderRadius: BorderRadius.circular(20),
+        border: Border.all(color: theme.tertiary),
+      ),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          Row(
+            children: [
+              Container(
+                decoration: BoxDecoration(
+                  color: theme.tertiary,
+                  borderRadius: BorderRadius.circular(999),
+                ),
+                padding: EdgeInsets.all(6),
+                child: Icon(icon, color: Colors.white, size: 32),
+              ),
+              SizedBox(width: 8),
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    title,
+                    style: TextStyle(
+                      color: theme.tertiary,
+                      fontWeight: FontWeight.bold,
+                      fontSize: 20,
+                    ),
+                  ),
+                  SizedBox(height: 4),
+                  Text(
+                    subtitle ?? "",
+                    style: TextStyle(color: theme.secondary),
+                  ),
+                ],
+              ),
+            ],
+          ),
+          SizedBox(width: 6),
+          Row(
+            children: [
+              Builder(
+                builder: (context) {
+                  if (isEditable) {
+                    return Container(
+                      decoration: BoxDecoration(
+                        color: theme.primary,
+                        borderRadius: BorderRadius.circular(999),
+                      ),
+                      child: IconButton(
+                        onPressed: onEdit,
+                        icon: Icon(Icons.edit_outlined, color: Colors.white),
+                      ),
+                    );
+                  } else {
+                    return SizedBox.shrink();
+                  }
+                },
+              ),
+              SizedBox(width: 4),
+              Builder(
+                builder: (context) {
+                  if (isDeletable) {
+                    return Container(
+                      decoration: BoxDecoration(
+                        color: Colors.red,
+                        borderRadius: BorderRadius.circular(999),
+                      ),
+                      child: IconButton(
+                        onPressed: onDelete,
+                        icon: Icon(Icons.delete_outlined, color: Colors.white),
+                      ),
+                    );
+                  } else {
+                    return SizedBox.shrink();
+                  }
+                },
+              ),
+            ],
+          ),
+        ],
+      ),
+    );
+  }
+}
--- MockTests/2 - Estudy/estudy/lib/features/admin/users_page.dart
+++ MockTests/2 - Estudy/estudy/lib/features/admin/users_page.dart
@@ -1,3 +1,5 @@
+import 'package:estudy/common/app_tile.dart';
+import 'package:estudy/services/auth_service.dart';
 import 'package:flutter/material.dart';
 
 class UsersPage extends StatefulWidget {
@@ -10,10 +12,49 @@ class UsersPage extends StatefulWidget {
 class _UsersPageState extends State<UsersPage> {
   @override
   Widget build(BuildContext context) {
+    final theme = Theme.of(context).colorScheme;
+    final AuthService authService = AuthService();
+
     return Scaffold(
       appBar: AppBar(
-        title: Text("Users Management"),
+        title: Text("Gerenciamento de Usuários"),
+        foregroundColor: theme.tertiary,
         backgroundColor: Colors.transparent,
+      ),
+      body: Expanded(
+        child: StreamBuilder(
+          stream: authService.getUsers(),
+          builder: (context, snapshot) {
+            if (snapshot.connectionState == ConnectionState.waiting) {
+              return Center(child: CircularProgressIndicator());
+            }
+            if (!snapshot.hasData) {
+              return Center(child: Text("Nenhum usuário encontrado!"));
+            }
+            if (snapshot.hasError) {
+              return Center(child: Text("Erro na busca de usuários"));
+            }
+
+            return ListView(
+              children: snapshot.data!
+                  .map<Widget>((userData) => _buildUserItem(userData, context))
+                  .toList(),
+            );
+          },
+        ),
+      ),
+    );
+  }
+
+  Widget _buildUserItem(Map<String, dynamic> userData, BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 25),
+      child: AppTile(
+        title: userData["email"],
+        subtitle: userData["user_level"],
+        icon: Icons.person_outline,
+        isDeletable: true,
+        isEditable: true,
       ),
     );
   }

57fdaa7192c6d8f57772464b199d26b817d5f57e
ppfuark <phfuark2007@gmail.com>
2026-01-28 16:20:54 -0300

    MockTests[chore]: Estudy
    organizing the folder and file structure

--- MockTests/2 - Estudy/estudy/lib/services/auth_gate.dart
+++ MockTests/2 - Estudy/estudy/lib/services/auth_gate.dart
@@ -1,6 +1,6 @@
 import 'package:estudy/features/auth/login_page.dart';
 import 'package:estudy/features/home/teacher_home.dart';
-import 'package:estudy/features/home_page.dart';
+import 'package:estudy/features/home/home_page.dart';
 import 'package:estudy/services/auth_service.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
